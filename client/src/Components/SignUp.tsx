37f7ea35d39d0e74f5dca1c76c2144ec4b082b10
Nadiia2323 <yariginanm@gmail.com>
2023-11-29 16:53:37 +0100

    Updated SignUp Component to include error handling functionality. Implemented validation checks for input fields and added error messages for better user feedback

--- client/src/Components/SignUp.tsx
+++ client/src/Components/SignUp.tsx
@@ -11,9 +11,17 @@ interface User {
   email: string;
   password: string;
 }
+interface Errors {
+  emailError?: string;
+  passwordError?: string;
+}
 
 export default function SignUp() {
-  const [errorMessage, setErrorMessage] = useState('')
+  const [showPassword, setShowPassword] = useState(false);
+  //  const [password, setPassword] = useState("");
+  // const [repeatPassword, setRepeatPassword] = useState("");
+
+  const [errorMessage, setErrorMessage] = useState<Errors>();
   const [hasAccount, setHasAccount] = useState(false);
   const [selectedCategory, setSelectedCategory] = useState("");
   const [newTheater, setNewTheater] = useState<Theater>({
@@ -27,36 +35,74 @@ export default function SignUp() {
     email: "",
     password: "",
   });
+  const toggleVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
-const isValidEmail = (email: string) => {
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return emailRegex.test(email);
-};
+  const isValidEmail = (email: string) => {
+    console.log('email :>> ', email);
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+  //   const validatePassword = (password, repeatPassword) => {
+  //   return password === repeatPassword;
+  // };
 
-const handelRegisterOnChange = (e: ChangeEvent<HTMLInputElement>) => {
-  const { name, value } = e.target;
+  const handelRegisterOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
 
-  if (selectedCategory === "Theater" && name === "email") {
-    if (!isValidEmail(value)) {
-      
-      console.log("Invalid email format!");
-      setErrorMessage("Invalid email format!")
-    } else {
-      setErrorMessage("");
+    if (selectedCategory === "Theater" && name === "email") {
+      if (!isValidEmail(value)) {
+        console.log("Invalid email format!");
+        setErrorMessage({ emailError: "Invalid email format!" });
+      } else {
+        setErrorMessage({});
+      }
+      console.log('name :>> ', name);
+      console.log('value :>> ', value);
       setNewTheater({ ...newTheater, [name]: value });
       setNewUser({ name: "", email: "", password: "" });
-    }
-  } else if (selectedCategory === "User" && name === "email") {
-    if (!isValidEmail(value)) {
-      setErrorMessage("Invalid email format!")
-    } else {
-      setErrorMessage("");
+    } else if (selectedCategory === "User" && name === "email") {
+      if (!isValidEmail(value)) {
+        setErrorMessage({ emailError: "Invalid email format" });
+      } else {
+        setErrorMessage({});
+      }
       setNewUser({ ...newUser, [name]: value });
       setNewTheater({ theaterName: "", email: "", password: "" });
-    }
-  }
-};
+    } else if (
+      (selectedCategory === "Theater" || selectedCategory === "User") &&
+      (name === "password" || name === "repeatPass")
+    ) {
+      setNewTheater({ ...newTheater, [name]: value });
+      const passwordField =
+        selectedCategory === "Theater" ? newTheater.password : newUser.password;
+      //  const repeatPassField = selectedCategory === "Theater" ? newTheater.repeatPass : newUser.repeatPass;
+      // if ("repeatPass" !== passwordField) {
+      const repeatPasswordValue = e.target.name === "repeatPass" ? e.target.value : ""
+      
+      if (repeatPasswordValue !== passwordField) {
 
+        console.log('passwordField :>> ', passwordField);
+        // console.log('selectedCategory :>> ', selectedCategory);
+        // console.log('newTheater.password :>> ', newTheater.password);
+        // console.log('newTheater.password :>> ', newUser.password);
+
+        
+       console.log("password NOT match");
+        setErrorMessage({ passwordError: "Passwords do not match!" });
+      } else {
+        console.log("password MATCH");
+        if (selectedCategory === "Theater") {
+          setErrorMessage({});
+          setNewTheater({ ...newTheater, [name]: value });
+        } else {
+          setErrorMessage({});
+          setNewUser({ ...newUser, [name]: value });
+        }
+      }
+    }
+  };
 
   const heading = !hasAccount ? "SignUp" : "SignIn";
   const buttonLabel: string = !hasAccount
@@ -70,24 +116,23 @@ const handelRegisterOnChange = (e: ChangeEvent<HTMLInputElement>) => {
   function handleSelect(category: string) {
     setSelectedCategory(category);
   }
-  
 
   const registration = async (data: Theater | User, url: String) => {
-    console.log('newTheater :>> ', newTheater);
-   
+    console.log("newTheater :>> ", newTheater);
+
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
 
     const urlencoded = new URLSearchParams();
-     if ('theaterName' in data) {
-    // It's Theater type
-    urlencoded.append("name", data.theaterName);
-  } else {
-    // It's User type
-    urlencoded.append("name", data.name);
-  }
-  urlencoded.append("email", data.email);
-  urlencoded.append("password", data.password);
+    if ("theaterName" in data) {
+      // It's Theater type
+      urlencoded.append("name", data.theaterName);
+    } else {
+      // It's User type
+      urlencoded.append("name", data.name);
+    }
+    urlencoded.append("email", data.email);
+    urlencoded.append("password", data.password);
 
     const requestOptions = {
       method: "POST",
@@ -101,11 +146,11 @@ const handelRegisterOnChange = (e: ChangeEvent<HTMLInputElement>) => {
       .catch((error) => console.log("error", error));
   };
   const handleRegistration = async () => {
-  const data = selectedCategory === 'Theater' ? newTheater : newUser;
-  const endpoint = selectedCategory === 'Theater' ? 'theaters/register' : 'users/register';
+    const data = selectedCategory === "Theater" ? newTheater : newUser;
+    const endpoint =
+      selectedCategory === "Theater" ? "theaters/register" : "users/register";
     await registration(data, endpoint);
-
-};
+  };
 
   // }
   return (
@@ -146,27 +191,36 @@ const handelRegisterOnChange = (e: ChangeEvent<HTMLInputElement>) => {
                   name="email"
                   onChange={handelRegisterOnChange}
                 />
-                {errorMessage && (<p className="errorMessage">{errorMessage}</p>)}
+                {errorMessage && (
+                  <p className="errorMessage">{errorMessage.emailError}</p>
+                )}
                 <div className="pass">
-                <label htmlFor="password">Password</label>
-                <span className="icon-pass">&#128274;</span>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  onChange={handelRegisterOnChange}
+                  <label htmlFor="password">Password</label>
+                  <span className="icon-pass" onClick={toggleVisibility}>
+                    &#128274;
+                  </span>
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    name="password"
+                    id="password"
+                    onChange={handelRegisterOnChange}
                   />
-                  </div>
+                </div>
                 <div className="pass">
-                <label htmlFor="repeatPass">Repeat Password</label>
-                <span className="icon-pass">&#128274;</span>
-                <input
-                  type="password"
-                  name="repeatPass"
-                  id="repeatPass"
-                  onChange={handelRegisterOnChange}
+                  <label htmlFor="repeatPass">Repeat Password</label>
+                  <span className="icon-pass" onClick={toggleVisibility}>
+                    &#128274;
+                  </span>
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    name="repeatPass"
+                    id="repeatPass"
+                    onChange={handelRegisterOnChange}
                   />
-                  </div>
+                  {errorMessage && (
+                    <p className="errorMessage">{errorMessage.passwordError}</p>
+                  )}
+                </div>
               </div>
             </div>
           )}
@@ -183,7 +237,12 @@ const handelRegisterOnChange = (e: ChangeEvent<HTMLInputElement>) => {
               </div>
               <div className="reg-form">
                 <label htmlFor="userName">User Name</label>
-                <input type="text" id="userName" name="name" onChange={handelRegisterOnChange}/>
+                <input
+                  type="text"
+                  id="userName"
+                  name="name"
+                  onChange={handelRegisterOnChange}
+                />
                 <label htmlFor="emailUser">Email</label>
 
                 <input
@@ -192,22 +251,32 @@ const handelRegisterOnChange = (e: ChangeEvent<HTMLInputElement>) => {
                   name="email"
                   onChange={handelRegisterOnChange}
                 />
-                {errorMessage && (<p className="errorMessage">{errorMessage}</p>)}
+                {errorMessage && (
+                  <p className="errorMessage">{errorMessage.emailError}</p>
+                )}
                 <div className="pass">
-                <label htmlFor="passwordUser">Password</label>
-                <span className="icon-pass">&#128274;</span>
-                <input
-                  type="password"
-                  id="passwordUser"
+                  <label htmlFor="passwordUser">Password</label>
+                  <span className="icon-pass" onClick={toggleVisibility}>
+                    &#128274;
+                  </span>
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    id="passwordUser"
                     name="password"
-                  onChange={handelRegisterOnChange}
+                    onChange={handelRegisterOnChange}
                   />
-                  </div>
+                </div>
                 <div className="pass">
-                <label htmlFor="Pass">Repeat Password</label>
-                <span className="icon-pass" >&#128274;</span>
-                  <input type="password" id="Pass" />
-                  </div>
+                  <label htmlFor="Pass">Repeat Password</label>
+                  <span className="icon-pass" onClick={toggleVisibility}>
+                    &#128274;
+                  </span>
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    id="Pass"
+                    name="repeatPass"
+                  />
+                </div>
               </div>
             </div>
           )}
@@ -221,7 +290,7 @@ const handelRegisterOnChange = (e: ChangeEvent<HTMLInputElement>) => {
         </div>
       )}
 
-      <button onClick={handleRegistration} >{registerButton}</button>
+      <button onClick={handleRegistration}>{registerButton}</button>
       <button onClick={toggleHasAccount}>{buttonLabel}</button>
     </div>
   );
